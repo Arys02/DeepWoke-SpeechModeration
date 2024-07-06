f1ad11c8cde8a20c449aea83028bd0f18f6844fe
Stephan <sschweitzer2@myges.fr>
2024-07-06 20:56:41 +0200

    beginning seperation of sending and receiving information to make discord interactions more modular

--- bot_discord/discord_bot.py
+++ bot_discord/discord_bot.py
@@ -1,14 +1,24 @@
 import discord
 import os
 import requests
+import websocket
+import json
 
-bot_url = "http://90.60.20.92:8000/classify/"
+# bot_url = "http://90.60.20.92:8000/classify/"
+# bot_url = "ws://localhost:3001"
+bot_url = "ws://localhost:3001"
 
 
-def send_to_model_api(message) -> float:
-    message_obj = {"text": message}
-    response = requests.post(bot_url, json=message_obj)
-    return float(response.json()['class'])
+def send_to_model_api(message):
+    message_data = {
+        "type": "inbound",
+        "text": message.content,
+        "user": str(message.author),
+        "is_hateful": None
+    }
+
+    ws.send(json.dumps(message_data))
+
 
 
 from dotenv import load_dotenv, dotenv_values
@@ -19,26 +29,26 @@ load_dotenv()
 # accessing and printing value
 token = os.getenv("DISCORD_API")
 
+ws = websocket.WebSocket()
+ws.connect(bot_url)
+
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
-
 
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user} (ID: {client.user.id})')
     print('------')
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     print(message.author)
     print(message.content)
-    response = send_to_model_api(message.content)
-    print(response)
-    if response > 0.5:
-        await message.channel.send("PROBLÉMATIQUE !!!")
-
+    send_to_model_api(message)
 
 client.run(token)
-#%%
+# %%
