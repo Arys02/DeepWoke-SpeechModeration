eba37c3d587abeb650fe76b8f9b2b47657b2d6a1
arys <charles.yaiche.lf@gmail.com>
2024-07-09 20:33:17 +0200

    add new formatted with fasttext
    add new formatted with fasttext
    
    update models and add vit
    
    update models and add vit
    
    update
    
    auie
    
    remove idea

--- ml/ml_core/src/core/params_lists.py
+++ ml/ml_core/src/core/params_lists.py
@@ -1,5 +1,19 @@
 available_params = {
-    "model": ['convnet', 'mlp'],
-    'embedder': ['camembert', 'bert', 'fasttext'],
-    'embedding_data': ["almanach-camembert-base", "small_bert_bert_en_uncased_L-4_H-512_A-8", "cc.fr.300.bin"]
+    "model": [
+        'convnet',
+        'mlp',
+        'bilstm',
+        'rnn',
+        'lstm'
+    ],
+    'embedder': [
+        'camembert',
+        'bert',
+        'fasttext'
+    ],
+    'embedding_data': [
+        "almanach-camembert-base",
+        "small_bert_bert_en_uncased_L-4_H-512_A-8",
+        "cc.fr.300.bin"
+    ]
 }
--- ml/ml_core/src/model/bert_mlp_builder.py
+++
@@ -1,3 +0,0 @@
-import keras_core as keras
-import tensorflow as tf
-import tensorflow_hub as hub
